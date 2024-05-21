038947579540a2e023f7729416d5c4021557c918
inklemon1412 <jonasnie@getacademy.no>
2024-05-21 16:50:57 +0200

    Add project files.

---
+++ Nyrehjelpen/Kidneys.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nyrehjelpen
+{
+    internal class Kidneys
+    {
+        public string KidneyName { get; private set; }
+        public int KidneyHealth { get; set; }
+      
+        
+        bool isHealthy = false;
+
+        public Kidneys(string kidneyName, int kidneyHealth)
+        {
+            KidneyName = kidneyName;
+            KidneyHealth = kidneyHealth;
+            
+           
+          
+        }
+        public void addKidney()
+        {
+          
+            string input = Console.ReadLine();
+
+            if(input == "0" ) 
+            {
+             
+                Console.WriteLine("Bernt has died. Good job");
+            }
+            else if (input == "1")
+            {
+                Console.WriteLine("You have chosen the kidney with a health of" + KidneyHealth + "You should probably not choose this one");
+            }
+            else if(input == "2")
+            {
+                Console.WriteLine("You have chosen the kidney with a health of" + KidneyHealth + "You should probably choose this one");
+            }
+            
+        }
+    }
+}
---
+++ Nyrehjelpen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Nyrehjelpen
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+
+        {            
+            var oldKidney = new Kidneys("old kidney", 2);
+            var newKidney1 = new Kidneys("new kidney", 60);
+            var newKidney2 = new Kidneys("new kidney", 100);
+
+            Console.WriteLine($"Bernt has been in an accident! His {oldKidney.KidneyName} is at a health level of {oldKidney.KidneyHealth}");
+            Console.WriteLine($"Luckily his cousin Kåre has a healthy pair that he can donate. One of them is at a health level of {newKidney1.KidneyHealth} and the other {newKidney2.KidneyHealth}");
+            Console.WriteLine($"Which one should he donate? Press '1' or '2' and see what happens");
+
+            newKidney1.addKidney();
+            newKidney2.addKidney();
+        }
+        
+    }
+}
+
+
